1d98d14fa8baac288fc57efda1624b8fb51eb7d4
IAmMrLuck <114543523+IAmMrLuck@users.noreply.github.com>
2023-10-22 20:06:55 +1100

    Wingsword blocks turret bullets

--- bullets/sword_bullet.gd
+++ bullets/sword_bullet.gd
@@ -1,11 +1,13 @@
+class_name WingSwordBullet
 extends Bullet
 
 @export var bullet_lifetime: float = 2
 
-@onready var lifespan_timer = $Timer
+@onready var lifespan_timer = $LifespanTimer
 
 func _ready():
 	lifespan_timer.wait_time = bullet_lifetime
+	speed = 100
 
 func _on_timer_timeout():
 	queue_free()
@@ -17,8 +19,6 @@ func _on_body_entered(body):
 
 	if child_node != null and child_node.has_method("handle_damage"):
 		child_node.handle_damage(bullet_damage)
-
-
 
 
 
