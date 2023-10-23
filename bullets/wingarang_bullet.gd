4fc6293913bb357506bab75b2fcc415f7af884e2
IAmMrLuck <114543523+IAmMrLuck@users.noreply.github.com>
2023-10-24 10:02:40 +1100

    work done with Wingarang

--- bullets/wingarang_bullet.gd
+++ bullets/wingarang_bullet.gd
@@ -2,8 +2,7 @@ extends Bullet
 
 var rotation_speed = 10
 
-func _ready():
-	direction = Vector2.ZERO
+var wingarang_end_marker = preload("res://weapons/wingarang/wingarang_gun.tscn")
 
 func _process(delta):
 	$".".rotate(1 * rotation_speed * delta)
@@ -16,3 +15,15 @@ func _on_body_entered(body):
 
 	if child_node != null and child_node.has_method("handle_damage"):
 		child_node.handle_damage(bullet_damage)
+
+
+func _on_visible_on_screen_notifier_2d_screen_exited():
+	direction.x = global_position.direction_to(GlobalPlayerInfo.player_position).x *2 
+	direction.y = global_position.direction_to(GlobalPlayerInfo.player_position).y *2
+#
+#	self.direction = Vector2(
+#		GlobalPlayerInfo.player_position.x - self.x, 
+#		GlobalPlayerInfo.player_position.y - self.y
+#		)
+#
+
