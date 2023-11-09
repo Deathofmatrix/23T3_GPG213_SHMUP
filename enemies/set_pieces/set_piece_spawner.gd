a3ecef5f6422431a034902b138268483c386c32d
Deathofmatrix <twinsy4@gmail.com>
2023-11-09 17:45:55 +1100

    base of meteor setpiece

--- enemies/set_pieces/set_piece_spawner.gd
+++ enemies/set_pieces/set_piece_spawner.gd
@@ -1,18 +1,31 @@
 extends Node2D
 
+var setpieces: Array = [
+	preload("res://enemies/set_pieces/meteor_setpiece.tscn"),
+	]
+
+
 func _ready():
 	EventManager.connect("difficulty_level_changed", spawn_at_difficulty)
 
 
 func spawn_at_difficulty(difficulty_level):
 	match difficulty_level:
-		4:
-			spawn_setpiece()
-		8:
-			spawn_setpiece()
+		2:
+			choose_set_piece()
+		5:
+			choose_set_piece()
+		9:
+			choose_set_piece()
 		_:
 			pass
 
 
-func spawn_setpiece():
-	pass
+func choose_set_piece():
+	var chosen_set_piece = setpieces.pick_random()
+	spawn_set_piece(chosen_set_piece)
+
+
+func spawn_set_piece(set_peice_to_spawn):
+	var set_piece_instance = set_peice_to_spawn.instantiate()
+	add_child(set_piece_instance)
--- obstacles/obstacle_spawner.gd
+++ obstacles/obstacle_spawner.gd
@@ -3,6 +3,7 @@ extends Node2D
 @export var current_obstacle_speed = 100
 @export var chance_to_spawn_obstacle = 0.9
 @export var chance_to_spawn_2_obstacle = 0.3
+@export var obstacle_spawn_cooldown = 5
 
 var obstacle_scene: PackedScene = preload("res://obstacles/meteor.tscn")
 var min_x_spawn: float
@@ -10,10 +11,13 @@ var max_x_spawn: float
 
 @onready var marker_min_x_spawn = $Markers/MarkerMinXSpawn
 @onready var marker_max_x_spawn = $Markers/MarkerMaxXSpawn
+@onready var creation_timer = $CreationTimer
+
 
 func _ready():
 	min_x_spawn = marker_min_x_spawn.position.x
 	max_x_spawn = marker_max_x_spawn.position.x
+	creation_timer.wait_time = obstacle_spawn_cooldown
 
 
 func create_obstacle():
