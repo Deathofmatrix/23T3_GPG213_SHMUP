8da22d72ed9529a272a6698c95c2d561535614f0
IAmMrLuck <114543523+IAmMrLuck@users.noreply.github.com>
2023-11-14 20:42:06 +1100

    boss playtesting underway

---
+++ bosses/BossOne.gd
@@ -0,0 +1,30 @@
+extends Enemy
+
+var can_move = false
+var slide_speed = 10
+
+@onready var health_system = $HealthSystem
+
+func _process(_delta):
+	if  health_system.health <= 500:
+		$StateChart.send_event("phase_two")
+	
+
+func _physics_process(delta):
+	if can_move == true:
+		position.x += slide_speed * delta
+	
+		if global_position.x >= 100:
+			slide_speed -= 1
+		if global_position.x <= 0:
+			slide_speed += 1
+
+
+
+func _on_phase_two_state_entered():
+	$Phases/PhaseTwo/BossShotgun.can_shoot = true
+	$Phases/PhaseTwo/BossShotgun2.can_shoot = true
+	$Phases/PhaseTwo/BossShotgun3.can_shoot = true
+	
+	can_move = true
+
---
+++ bosses/boss_weapons/boss_shotgun.gd
@@ -0,0 +1,40 @@
+extends Node2D
+
+var Bullet = preload("res://enemies/enemy_bullets/turret_bullet.tscn")
+
+
+var bullet_damage = 10
+var can_shoot = false
+
+@onready var bullet_spawns = $BulletSpawns
+@onready var shoot_speed_timer = $BossShotTimer
+
+
+func _process(_delta):
+	if can_shoot == true:
+#		print(can_shoot)
+		shoot_bullet()
+
+
+func _on_boss_shot_timer_timeout():
+	can_shoot = true
+
+
+func shoot_bullet():
+#	print("shoot_bullet called")
+	for spawn_point in bullet_spawns.get_children():
+#		print("found children")
+		spawn_bullet(spawn_point)
+	shoot_speed_timer.start()
+	can_shoot = false
+
+func spawn_bullet(spawn_point):
+	var bullet = Bullet.instantiate() as Bullet
+	bullet.position = spawn_point.global_position
+	var temp_direction = Vector2.DOWN.normalized()
+	bullet.direction = temp_direction.rotated(spawn_point.rotation)
+	bullet.rotation = bullet.direction.angle()
+	bullet.bullet_damage = bullet_damage
+	get_parent().add_child(bullet)
+	return bullet
+
