5e2c36fec457eee9494a22aeea8cc9fa8bc9d288
Charlie SAE <114542867+Deathofmatrix@users.noreply.github.com>
2023-11-22 16:15:35 +1100

    Added miniboss
    keep the meteors in the scene

--- enemies/set_pieces/follow_enemy_miniboss/follow_enemy_miniboss.gd
+++ enemies/set_pieces/follow_enemy_miniboss/follow_enemy_miniboss.gd
@@ -1,17 +1,39 @@
 class_name FollowEnemyMiniboss
 extends Enemy
 
-var direction = Vector2(0,1)
+enum STATE {FOLLOWING, CHARGING}
+
+var current_state = STATE.FOLLOWING
+var direction = Vector2.ZERO
+var last_player_direction = Vector2.ZERO
 
 @export var movement_speed: int = 50
+@export var charge_speed: int = 250
 
 func _process(_delta):
 	direction = global_position.direction_to(GlobalPlayerInfo.player_position)
 	look_at(GlobalPlayerInfo.player_position)
 
 func _physics_process(_delta):
-	velocity = direction * movement_speed
+	match current_state:
+		STATE.FOLLOWING:
+			follow_player()
+		STATE.CHARGING:
+			charge_at_player()
+	
 	move_and_slide()
+
+
+func follow_player():
+	print("following")
+	velocity = direction * movement_speed
+
+
+func charge_at_player():
+	print("charging")
+	velocity = last_player_direction * charge_speed
+	await get_tree().create_timer(1).timeout
+	current_state = STATE.FOLLOWING
 
 
 func _on_health_system_killed():
@@ -25,5 +47,14 @@ func _on_health_system_health_updated(_health, _was_damaged):
 	enemy_hit_player.play()
 
 
+func _on_charge_cooldown_timeout():
+	last_player_direction = direction
+	current_state = STATE.CHARGING
+
+
+func _on_visible_on_screen_notifier_2d_screen_entered():
+	movement_speed = 50
+
+
 func _on_visible_on_screen_notifier_2d_screen_exited():
-	queue_free()
+	movement_speed = 200
